9d7104cb29473a134c6426d5f517200a8d4ba224
GabrielDuarteSz <gabriel_eduarte@soy.sena.edu.co>
2025-03-17 18:42:02 -0500

    prueba de subida

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const productosRoutes = require('./routes/productos');
 const reseniaRoutes = require('./routes/Resenia');
 const dotenv = require('dotenv');
 
-dotenv.config(); // Cargar variables de entorno
+dotenv.config(); // Cargar variables de entorno!
 
 const app = express();
 const port = process.env.PORT || 3000;
